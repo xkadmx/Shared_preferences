4066a205bcf0c4e02aaacadd098f5aec8447efb2
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-21 16:50:24 +0200

    setting execute() on AsyncTest object

--- app/src/main/java/com/example/shared_preferences/MainActivity.java
+++ app/src/main/java/com/example/shared_preferences/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTest().execute();
     }
     private class AsyncTest extends AsyncTask<Void, Void, Void>{
 
